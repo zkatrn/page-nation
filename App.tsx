0c74208b2aa1020449fded1f964e79f5ae3f3b0b
zkatrn <kathrynrandrade@gmail.com>
2022-10-08 13:26:00 -0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>My Pages</h1>
+      <p>Hello, World :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# page-nation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-4webrk)
